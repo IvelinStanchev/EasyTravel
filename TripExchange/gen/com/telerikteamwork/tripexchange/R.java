ca9f00327234ee88e3f23a6ca212e715536425e1
IvelinStanchev <telerika123@gmail.com>
2014-10-11 14:59:57 +0300

    Added login and register forms

--- TripExchange/gen/com/telerikteamwork/tripexchange/R.java
+++ TripExchange/gen/com/telerikteamwork/tripexchange/R.java
@@ -1091,10 +1091,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int btn_register=0x7f05003f;
         public static final int checkbox=0x7f05002e;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1103,6 +1104,8 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050031;
         public static final int end=0x7f050013;
+        public static final int et_password=0x7f05003e;
+        public static final int et_username=0x7f05003d;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002d;
         public static final int home=0x7f050014;
@@ -1194,7 +1197,9 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int login_form=0x7f030019;
+        public static final int register_form=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
